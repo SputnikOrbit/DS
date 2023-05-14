ee056f511c13b71940bbc018f82135cb5e952f34
sputnik <ysy1054917144@outlook.com>
2023-05-14 15:43:49 +0800

    i start to be akward

--- Polynominal.c
+++ Polynominal.c
@@ -187,10 +187,13 @@ int main()
     return 0;
 }
 
-void git_bug_set()
+void git_debug_test()
 {
-    printf("I Love U So Don't bring me trouble");
+    int a = 255;
+    char ch = "git I don't know";
+    
 }
+
 // 这道题我决定采用链表构造多项式，创造虚拟头节点的同时
 // 要设计方法：初始化、插入
 // 和多项式部分，直接将a的每一项插入到b中合适位置即可
